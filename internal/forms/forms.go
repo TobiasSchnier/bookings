7325ee1f201fb935c2773ce4da9166b64d0b1156
Tobias Schnier <tobias@frielingsdorf-datenservice.de>
2023-05-17 08:49:35 +0200

    server side validation

---
+++ internal/forms/forms.go
@@ -0,0 +1,47 @@
+package forms
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "Field cannot be Blank MOTHERFUCKER!")
+		}
+	}
+}
+
+func (f *Form) Has(field string, r *http.Request) bool {
+	x := r.Form.Get(field)
+	return x != ""
+}
+
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("Field must be at least %d charaters long you idiot", length))
+		return false
+	}
+	return true
+}
--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/TobiasSchnier/bookings/internal/forms"
+
 // template Data holds data set from Handler send to template
 type TemplateData struct {
 	StringMap map[string]string
@@ -10,4 +12,5 @@ type TemplateData struct {
 	Flash     string
 	Warning   string
 	Error     string
+	Form      *forms.Form
 }
